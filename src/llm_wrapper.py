6c50c26ebb21947efcd324d9d91aec1ff1768d28
Anton <10177340+anforsm@users.noreply.github.com>
2023-12-03 09:45:46 +0000

    results and fixes

--- src/llm_wrapper.py
+++ src/llm_wrapper.py
@@ -1,6 +1,8 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer 
+import torch
 from conf import *
 
+device = "cuda" if torch.cuda.is_available() else "cpu"
 model = AutoModelForCausalLM.from_pretrained(MODEL_1)
 tokenizer = AutoTokenizer.from_pretrained(MODEL_1)
 
@@ -8,7 +10,7 @@ def call_llm(prompt, model=model, tokenizer=tokenizer):
   max_len = 2048
   if len(prompt) > max_len:
     print(f"Warning: prompt is too long {len(prompt)}, truncating to {max_len}")
-  tokens = tokenizer.encode(prompt[-1024:], return_tensors="pt")
+  tokens = tokenizer.encode(prompt[-1024:], return_tensors="pt").to(device)
   generated_ids = model.generate(tokens, pad_token_id=tokenizer.eos_token_id, max_new_tokens=100)
   output = tokenizer.decode(generated_ids[0], skip_special_tokens=True)
   return output[len(prompt):]
--- src/readability.py
+++ src/readability.py
@@ -6,6 +6,7 @@ from tqdm import tqdm
 import json
 import argparse
 from transformers import AutoModelForCausalLM, AutoTokenizer 
+import torch
 
 from readability_prompts import COUNT_VAR_PROMPT, PROMPT_CRITIQUE, PROMPT_FIX
 
@@ -13,9 +14,9 @@ def self_refine(code, model, tokenizer):
   code = code.replace("\n\n", "\n")
   code_prompt = PROMPT_CRITIQUE.format(code=code)
   #feedback = call_openai(code_prompt)
-  feedback = call_llm(code_prompt, model)
+  feedback = call_llm(code_prompt, model, tokenizer)
   fix_code_prompt = PROMPT_FIX.format(code=code, suggestion=feedback)
-  new_code = call_llm(fix_code_prompt, model)
+  new_code = call_llm(fix_code_prompt, model, tokenizer)
   #new_code = call_openai(fix_code_prompt)
   return feedback, new_code
   
@@ -24,7 +25,7 @@ def main(model_name, model, tokenizer):
   programs = pd.read_json("data/code_samples/codenet-python-test-1k.jsonl", lines=True, orient="records")
   results = []
   processed_programs = set()
-  num_sampes = 3
+  num_sampes = len(programs)
   for i, row in tqdm(programs.iterrows(), total=num_sampes):
     id = row["submission_id_v0"]
     if id in processed_programs:
@@ -58,10 +59,12 @@ def main(model_name, model, tokenizer):
 
 if __name__ == "__main__":
   # get modle name
+  device = "cuda" if torch.cuda.is_available() else "cpu"
   parser = argparse.ArgumentParser()
   parser.add_argument("--model", type=str, default="EleutherAI/gpt-neo-1.3B")
   args = parser.parse_args()
   model_name = args.model
   model = AutoModelForCausalLM.from_pretrained(args.model)
+  model = model.to(device)
   tokenizer = AutoTokenizer.from_pretrained(args.model)
   main(model_name, model, tokenizer)
